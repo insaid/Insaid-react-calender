beec5a5f20d919b756a77a81f09a37f24db7f888
unknown <shubham@insaid.co>
2023-04-27 18:39:44 +0530

    text add

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -202,6 +202,9 @@ else{
                       <Typography sx={{ textAlign: "center", py: 2 }}>
                         Login
                       </Typography>
+                       <Typography sx={{ textAlign: "center", py: 2 }}>
+                       Hii admin
+                      </Typography>
                     </Box>
                     <Box sx={{ mx: 2, py: 5 }}>
                       <form onSubmit={HandleLogin}>
