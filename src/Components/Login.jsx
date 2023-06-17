ec92b953840baf19a454baa66af0a67186088301
unknown <shubham@insaid.co>
2023-06-17 09:29:15 +0530

    login page chnages

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -107,7 +107,7 @@ if(id){
       password: CryptoJS.AES.encrypt(password,'INSAID@login$2022@$newweb$@').toString(),
     };
     axios
-      .post("https://www.insaid.co/wp-json/jwt-auth/v1/token", loginData)
+      .post(`${siteUrl}/login`, JSON.stringify(loginData),options,{ withCredentials: true, })
       .then((res) => {
         console.log(res.data, "auth");
         localStorage.setItem("email", username);
